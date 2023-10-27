3a448ea52eb41f5785b7a842e3d3abdb75375de5
dukie <dukie@DESKTOP-JO2T7SL>
2023-10-27 15:25:37 +0530

    First commit : Calc.java is added

---
+++ src/com/edubridge/app1/Calc.java
@@ -0,0 +1,18 @@
+package com.edubridge.app1;
+
+
+public class Calc {
+	
+	public static void findSquare(int num)
+	{
+		
+		System.out.println(num*num);
+		
+	}
+	public static void findCube(int num)
+	{
+		
+		System.out.println(num*num*num);
+		
+	}
+}
